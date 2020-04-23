b30ab980c688bb678736fc072fb38c1467ccd97b
bruce <bruce@localhost.localdomain>
2020-04-23 07:24:38 +0100

    remove uri mapping

--- calendar/src/main/bash/listItems.sh
+++ calendar/src/main/bash/listItems.sh
@@ -20,9 +20,9 @@ args=${*:--o https://uat.api.symphony.com -p https://psdev.symphony.com/ -s alle
 #args=$defaultArgs
 echo args $args
 
-mvn exec:java -Dexec.mainClass="com.symphony.s2.allegro.examples.calendar.ListItems" -Dexec.args="$args"
+#mvn exec:java -Dexec.mainClass="com.symphony.s2.allegro.examples.calendar.ListItems" -Dexec.args="$args"
 
-#java -cp target/calendar-0.1.3-SNAPSHOT-jar-with-dependencies.jar com.symphony.s2.allegro.examples.calendar.ListItems $args
+java -cp target/calendar-0.1.3-SNAPSHOT-jar-with-dependencies.jar com.symphony.s2.allegro.examples.calendar.ListItems $args
 
 
 
